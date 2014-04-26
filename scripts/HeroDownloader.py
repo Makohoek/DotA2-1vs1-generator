6b361c2fb0ed80192e879b1625f007a12d9b5d8d
Mattijs Korpershoek <mattijs.korpershoek@gmail.com>
2014-04-26 13:15:54 +0200

    Python script to download hero portraits
    Finished the python script.
    It now also generates the HeroDatabase.txt in a format which the Java
    application understands, i.e. one heroName per line.
    
    Signed-off-by: Mattijs Korpershoek <mattijs.korpershoek@gmail.com>

--- scripts/HeroDownloader.py
+++ scripts/HeroDownloader.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python2
 
 from __future__ import print_function
 
-import urllib2
+import urllib
 import re
 
 class HeroDownloader(object):
@@ -10,7 +10,7 @@ class HeroDownloader(object):
         self.baseLink = heroPage
 
     def parseHeroLinks(self):
-        stream = urllib2.urlopen(self.baseLink)
+        stream = urllib.urlopen(self.baseLink)
         htmlPage = stream.read()
         self._heroLinks = re.findall('http://cdn.dota2.com/apps/dota2/images/heroes/[a-z_]*\_sb.png', htmlPage)
         self._replaceWithFullResolutionLinks()
@@ -30,10 +30,11 @@ class HeroDownloader(object):
         heroName = re.sub('_full.png', '', heroName)
         return heroName
 
-    #def createHeroDatabase(self): #FIXME: WIP here
-    #    newDatabase = open('HeroDatabase.txt', 'w')
-    #    print(self.getHeroNames(), file=newDatabase)
-    #    newDatabase.close()
+    def createHeroDatabase(self):
+        newDatabase = open('HeroDatabase.txt', 'w')
+        for heroName in self.getHeroNames():
+            print(heroName, file=newDatabase)
+        newDatabase.close()
 
     def downloadAllHeroPortraits(self):
         map (self._saveHeroPortraitToDisc, self._fullImageUrls)
@@ -44,7 +45,7 @@ class HeroDownloader(object):
         self._saveUrlToFile(imageUrl, pathToFile)
 
     def _saveUrlToFile(self, url, filePath):
-        fileStream = urllib2.urlopen(url)
+        fileStream = urllib.urlopen(url)
         newFile = open(filePath, 'w')
         newFile.write(fileStream.read())
         newFile.close()
@@ -60,4 +61,6 @@ print('Started downloading all portraits ...')
 downloader.downloadAllHeroPortraits()
 print('Done downloading all portraits')
 
-#downloader.createHeroDatabase()
+print('Writing heroes to database ...')
+downloader.createHeroDatabase()
+print('Done writing heroes to database')
